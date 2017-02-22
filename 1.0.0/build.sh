1fd282629a62fcf4014dbe2366f6319627d0f826
Ciro Cavani <ciro.cavani@gmail.com>
2017-02-21 22:42:00 -0300

    TensorFlow 1.0 (Python 3.5)

--- 1.0.0/build.sh
+++ 1.0.0/build.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -eu
 
-rm -rf ~/tensorflow-0.11
+rm -rf ~/tensorflow-1.0
 
-git clone https://github.com/tensorflow/tensorflow.git -b r0.11 ~/tensorflow-0.11
+git clone https://github.com/tensorflow/tensorflow.git -b r1.0 ~/tensorflow-1.0
 
-cd ~/tensorflow-0.11
-
-git apply ../5143.patch
+cd ~/tensorflow-1.0
 
 export PYTHON_BIN_PATH=/usr/bin/python
+export CC_OPT_FLAGS="-march=native"
+export TF_ENABLE_XLA=1
+export TF_NEED_JEMALLOC=1
 export TF_NEED_GCP=0
 export TF_NEED_HDFS=1
+export TF_NEED_OPENCL=0
 export TF_NEED_CUDA=1
 export GCC_HOST_COMPILER_PATH=/usr/bin/gcc
 export TF_CUDA_VERSION=8.0
@@ -19,10 +21,8 @@ export CUDA_TOOLKIT_PATH=/usr/local/cuda-8.0
 export TF_CUDNN_VERSION=5
 export CUDNN_INSTALL_PATH=/usr/local/cuda-8.0
 export TF_CUDA_COMPUTE_CAPABILITIES=3.0
-echo "/usr/lib/python2.7/dist-packages" | ./configure
+echo "/usr/lib/python3/dist-packages" | ./configure
 
 bazel build -j 4 -c opt --config=cuda //tensorflow/tools/pip_package:build_pip_package
 
 bazel-bin/tensorflow/tools/pip_package/build_pip_package $HOME
-
-mv ~/tensorflow-0.11.0-py2-none-{any,linux_x86_64}.whl
--- setup.sh
+++ setup.sh
@@ -9,24 +9,24 @@ if [ ! -f build_deps/cudnn-8.0-linux-x64-v5.1.tgz ]; then
     exit 1
 fi
 
-if [ ! -f build_deps/cuda_8.0.44_linux-run ]; then
+if [ ! -f build_deps/cuda_8.0.61_375.26_linux-run ]; then
     curl -k -L \
-        https://developer.nvidia.com/compute/cuda/8.0/prod/local_installers/cuda_8.0.44_linux-run \
-        -o build_deps/cuda_8.0.44_linux-run
+        https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run \
+        -o build_deps/cuda_8.0.61_375.26_linux-run
 fi
 
-if [ ! -f build_deps/jdk-8u112-linux-x64.tar.gz ]; then
+if [ ! -f build_deps/jdk-8u121-linux-x64.tar.gz ]; then
     curl -k -L \
         -H "Cookie: oraclelicense=accept-securebackup-cookie" \
-        http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.tar.gz \
-        -o build_deps/jdk-8u112-linux-x64.tar.gz
+        http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-x64.tar.gz \
+        -o build_deps/jdk-8u121-linux-x64.tar.gz
 fi
 
-if [ ! -f build_deps/bazel-0.4.0-installer-linux-x86_64.sh ]; then
+if [ ! -f build_deps/bazel-0.4.4-installer-linux-x86_64.sh ]; then
     curl -k -L \
-        https://github.com/bazelbuild/bazel/releases/download/0.4.0/bazel-0.4.0-installer-linux-x86_64.sh \
-        -o build_deps/bazel-0.4.0-installer-linux-x86_64.sh
+        https://github.com/bazelbuild/bazel/releases/download/0.4.4/bazel-0.4.4-installer-linux-x86_64.sh \
+        -o build_deps/bazel-0.4.4-installer-linux-x86_64.sh
 fi
 
-chmod +x build_deps/cuda_8.0.44_linux-run
-chmod +x build_deps/bazel-0.4.0-installer-linux-x86_64.sh
+chmod +x build_deps/cuda_8.0.61_375.26_linux-run
+chmod +x build_deps/bazel-0.4.4-installer-linux-x86_64.sh
