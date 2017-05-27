80995ef645800a6533de6dadd12fcf58f522ab26
Ciro Cavani <ciro.cavani@gmail.com>
2017-05-27 13:13:21 -0300

    TensorFlow 1.2.0rc1

--- 1.2-cpu/run.sh
+++ 1.2-cpu/run.sh
@@ -3,7 +3,9 @@ set -eu
 
 cd `dirname "$0"`/..
 
-rm -rf tensorflow-1.2.0rc0-cp35-cp35m-linux_x86_64.whl
+TF_WHEEL=tensorflow-1.2.0rc1-cp35-cp35m-linux_x86_64.whl
+
+rm -rf $TF_WHEEL
 
 ./setup.sh
 
@@ -15,8 +17,6 @@ docker run -t \
   tensorflow_cpu/build/ubuntu1604:1.2 \
   /bin/bash --login /home/tensorflow/build.sh
 
-docker cp \
-  tensorflow_cpu_1.2:/home/tensorflow/tensorflow-1.2.0rc0-cp35-cp35m-linux_x86_64.whl \
-  .
+docker cp tensorflow_cpu_1.2:/home/tensorflow/$TF_WHEEL .
 
 docker rm tensorflow_cpu_1.2
