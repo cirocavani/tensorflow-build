4bdbb9fed4b2c21ec8e089e4e44d952d69996a5c
Ciro Cavani <ciro.cavani@gmail.com>
2017-11-02 23:52:47 -0200

    TensorFlow 1.4.0 (release)

--- 1.4-cpu/run.sh
+++ 1.4-cpu/run.sh
@@ -3,19 +3,22 @@ set -eu
 
 cd $(dirname "$0")/..
 
-TF_WHEEL=tensorflow-1.4.0rc1-cp36-cp36m-linux_x86_64.whl
+TF_WHEEL=tensorflow-1.4.0-cp36-cp36m-linux_x86_64.whl
 
 rm -rf $TF_WHEEL
 
 ./setup.sh
 
-docker build -t tensorflow_cpu/build/ubuntu1604:1.4 -f 1.4-cpu/Dockerfile .
+docker build -t \
+    tensorflow_cpu/build/ubuntu1604:1.4 \
+    -f 1.4-cpu/Dockerfile \
+    .
 
 docker run -t \
-  --name tensorflow_cpu_1.4 \
-  --user tensorflow \
-  tensorflow_cpu/build/ubuntu1604:1.4 \
-  /bin/bash --login /home/tensorflow/build.sh
+    --name tensorflow_cpu_1.4 \
+    --user tensorflow \
+    tensorflow_cpu/build/ubuntu1604:1.4 \
+    /bin/bash --login /home/tensorflow/build.sh
 
 docker cp tensorflow_cpu_1.4:/home/tensorflow/$TF_WHEEL .
 
--- 1.4-gpu/run.sh
+++ 1.4-gpu/run.sh
@@ -3,19 +3,22 @@ set -eu
 
 cd $(dirname "$0")/..
 
-TF_WHEEL=tensorflow_gpu-1.4.0rc1-cp36-cp36m-linux_x86_64.whl
+TF_WHEEL=tensorflow_gpu-1.4.0-cp36-cp36m-linux_x86_64.whl
 
 rm -rf $TF_WHEEL
 
 ./setup.sh
 
-docker build -t tensorflow_gpu/build/ubuntu1604:1.4 -f 1.4-gpu/Dockerfile .
+docker build -t \
+    tensorflow_gpu/build/ubuntu1604:1.4 \
+    -f 1.4-gpu/Dockerfile \
+    .
 
 docker run -t \
-  --name tensorflow_gpu_1.4 \
-  --user tensorflow \
-  tensorflow_gpu/build/ubuntu1604:1.4 \
-  /bin/bash --login /home/tensorflow/build.sh
+    --name tensorflow_gpu_1.4 \
+    --user tensorflow \
+    tensorflow_gpu/build/ubuntu1604:1.4 \
+    /bin/bash --login /home/tensorflow/build.sh
 
 docker cp tensorflow_gpu_1.4:/home/tensorflow/$TF_WHEEL .
 
